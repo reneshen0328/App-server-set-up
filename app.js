7c8630c2d59e1343ae47dec74ac18cf0c67e3f45
Rene Shen <rshen@box.com>
2022-01-12 14:47:12 -0800

    commit

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const { db, Menu, Restaurant, MenuItem, seed } = require("./index");
 
 const port = 8080;
 
+// async function seeding() {
+//   await seed();
+// }
+
+// seeding();
+
+// Parse json
+app.use(express.json());
+
 app.get("/", async (req, res) => {
   res.send("<h1>Hello!</h1>");
 });
@@ -14,8 +23,8 @@ app.get("/restaurants", async (req, res) => {
   res.json({ restaurants });
 });
 
-app.get("/menuitem/:id", async (req, res) => {
-  const menuitem = await MenuItem.findByPk(req.params.id);
+app.get("/menuitem", async (req, res) => {
+  const menuitem = await MenuItem.findAll();
   res.json({ menuitem });
 });
 
@@ -24,7 +33,30 @@ app.get("/menu/:id", async (req, res) => {
   res.json({ menu });
 });
 
+app.post("/restaurants", async(req,res) =>{
+    await Restaurant.create(req.body)
+    res.send("Created!")
+})
+
+app.put("/restaurants/:id", async(req,res) =>{
+    await Restaurant.update(
+        req.body,
+        {where:{id: req.params.id}}
+    )
+    res.send("Updated!")
+})
+
+app.delete("/restaurants/:id", async(req,res) =>{
+    await Restaurant.destroy(
+        {where:{id: req.params.id}}
+    )
+    res.send("Deleted!")
+})
+
+
+
 app.listen(port, async () => {
-  await seed();
+    await seed();
+  //   await db.sync({ force: true });
   console.log(`Server is listening on http://localhost:${port}`);
 });
